184934b79e757450f0e7fd76995a5c05ec01f294
Yucan You <yucan.you100223@outlook.de>
2026-03-27 13:45:15 +0100

    fixes 2

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = '/api';
 let currentProjects = [];
 let currentSkills = {};
 let currentEditingProject = null;
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api';
+const API_URL = '/api';
 
 // Dynamic content loaded from API
 let dynamicProjects = [];
